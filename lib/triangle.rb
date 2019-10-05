22d917c34436cefd62e954a4daa2aa8ea00522b9
Bernadette Davis <bsd231@nyu.edu>
2019-10-05 23:31:15 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,8 +9,13 @@ class Triangle
   end
 
   def kind
-    if (@side1 == @side2 && @side2 == @side3)
+    if (@side1 == 0 || @side2 == 0 || @side3 == 0)
+      raise TriangleError 
+    elsif (@side1 == @side2 && @side2 == @side3)
       return :equilateral
+    
+    end
 
   end
 end
+
